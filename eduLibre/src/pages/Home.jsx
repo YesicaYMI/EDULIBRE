e2a104162b78abdea7e38f184401b8500d8a52d8
YesicaYMI <yeyumeis30@hotmail.com>
2024-09-30 15:50:25 -0500

    Cambios correctos!

--- eduLibre/src/pages/Home.jsx
+++ eduLibre/src/pages/Home.jsx
@@ -41,35 +41,5 @@ function Home() {
     );
 }
 
-<<<<<<< HEAD
-export default Home;
-=======
-const BenefitsSection = () => (
-  <section className="benefits-section">
-    <div className="benefit">
-      <h2>Educación Accesible</h2>
-      <p>Accede a cursos gratuitos y refuerza tus conocimientos.</p>
-    </div>
-    <div className="benefit">
-      <h2>Estudia a tu Ritmo</h2>
-      <p>Organiza tu propio plan de estudio y sigue tu progreso.</p>
-    </div>
-    <div className="benefit">
-      <h2>Título Oficial</h2>
-      <p>Prepárate para obtener tu título de bachiller con nuestros recursos.</p>
-    </div>
-  </section>
-);
-
-const Home = () => (
-  <div>
-    <Header />
-    <h1 className="header-title">EduLibre</h1> {/* Asegúrate de que este título esté incluido */}
-    <HeroSection />
-    <BenefitsSection />
-    <Footer />
-  </div>
-);
 
 export default Home;
->>>>>>> 6889e6ce143a75420babdc1c29a5d0c0636f8f9e
